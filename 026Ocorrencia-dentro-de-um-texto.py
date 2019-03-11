f06fd3f21e08bac188e3b350b306b3cd7c9599a4
victoralmeida432 <vala@gmail.com>
2019-03-11 14:34:16 -0300

    Atualização

---
+++ 026Ocorrencia-dentro-de-um-texto.py
@@ -0,0 +1,10 @@
+#Exercício Python 026: Faça um programa que leia
+#  uma frase pelo teclado e mostre quantas vezes 
+# aparece a letra "A", em que posição ela aparece 
+# a primeira vez 
+# e em que posição ela aparece a última vez.
+
+frase = str(input("Digite uma frase: ")).upper()
+print("A letra A aparece {} vezes na frase.".format(frase.count("A")))
+print("A primeira letra A apareceu na posição {}".format(frase.find("A")+1))
+print("A última letra A apareceu na posicão {}".format(frase.rfind("A")+1))
