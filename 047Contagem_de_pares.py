8a949ebaeb89da18f228b8936a1a114b92746f32
vala@cesar.school <vala@cesar.school>
2019-04-30 15:07:16 -0300

    Att

---
+++ 047Contagem_de_pares.py
@@ -0,0 +1,7 @@
+# Exercício Python 047: Crie um programa que mostre na 
+# tela todos os números pares que estão no intervalo entre 1 e 50.
+
+for c in range(0,51,2):
+    #print("{},".format(c))
+    print(c, end = ', ')
+print("Fim.")
