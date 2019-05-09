073623f547250cc937ec5decd227a09475341194
Victor Almeida <vala@cesar.com>
2019-05-09 11:01:39 -0300

    Atualizando

--- 060Cálculo_do_fatorial.py
+++ 060Cálculo_do_fatorial.py
@@ -2,12 +2,12 @@
 # número qualquer e mostre o seu fatorial.
 # Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
 
-c = 1
-n = int(input("Digite um número para saber seu fatorial: "))
-while n != 1:
-    c = c*n
-    n = n - 1
-print(c)
+# c = 1
+# n = int(input("Digite um número para saber seu fatorial: "))
+# while n != 1:
+#     c = c*n
+#     n = n - 1
+# print(c)
 
 
 #from math import factorial
@@ -16,11 +16,11 @@ print(c)
 #print("O fatorial de {} é {}".format(n,f))
 
 
-#n = int(input("Digite um número para saber seu fatorial: "))
-#c = n
-#while c > 0:
-#   print("{}".format(c),end='')
-#   print(' x ' if c > 1 else ' = ',end='')
-#   f *= c
-#   c -= 1
-#print("{}".format(f))    
+n = int(input("Digite um número para saber seu fatorial: "))
+c = n
+while c > 0:
+  print("{}".format(c),end='')
+  print(' x ' if c > 1 else ' = ',end='')
+  f *= c
+  c -= 1
+print("{}".format(f))    
