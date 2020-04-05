0bdefc3e344bf7a6a54230e917a92e6b52b63806
Victor Almeida <38266337+victoralmeida432@users.noreply.github.com>
2020-04-04 23:30:57 -0300

    Update 071Simulador_de_caixa_eletrônico.py

--- 071Simulador_de_caixa_eletrônico.py
+++ 071Simulador_de_caixa_eletrônico.py
@@ -30,3 +30,41 @@ print("=-="*20)
 print("Volte sempre e tenha um ótimo dia.")
     
     
+'''
+
+
+# # -*- coding: utf-8 -*-
+
+valor = int(input(""))
+total = valor
+ced = 100
+totced = 0
+print(valor)
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced >= 0:
+            print("{} nota(s) de R$ {},00".format(totced,ced))
+        if ced == 100:
+            ced = 50
+        elif ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        elif ced == 5:
+            ced = 2
+        elif ced == 2:
+            ced = 1
+        elif ced == 1:
+            ced = 0
+        totced = 0
+        if total == 0:
+            # print("{} nota(s) de R$ {},00".format(totced,ced))
+            break
+            
+ 
+    
